39a96fa4b071db299a2929b518b9579ca05768d2
Bayzid <macbook@Bayzids-MacBook-Air.local>
2026-04-29 23:56:38 +0600

    add sora font and fix nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,36 @@
 import dollarImg from "../../assets/dollar_1.png";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+    <div className="navbar bg-base-100 ">
+      <div className="flex-1 mx-16 justify-center items-center">
+        <img src={logo} className=""></img>
       </div>
       <div className="flex justify-between items-center gap-2">
-        <span>0 Coin</span>
-        <img src={dollarImg} alt="" />
+        <div className="">
+          <ul className="flex justify-between items-center space-x-20">
+            <li>
+              {" "}
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#fixture">Fixture</a>{" "}
+            </li>
+            <li>
+              {" "}
+              <a href="#teams">Teams</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#schedules">Schedules</a>
+            </li>
+          </ul>
+        </div>
+        <div className="flex justify-between items-center mx-16 gap-2">
+          <span>0 Coin</span>
+          <img src={dollarImg} alt="" />
+        </div>
       </div>
     </div>
   );
--- src/components/homePage/Banner/Banner.jsx
+++ src/components/homePage/Banner/Banner.jsx
@@ -1,11 +1,15 @@
-import bannerImg from "../../../assets/bg-shadow.png"
+// import bannerImg from "../../../assets/bg-shadow.png"
+// import bannerLogo from "../../../assets/banner-main.png"
+import banner from "../../../assets/banner-shadow.png";
 
 const Banner = () => {
-    return (
-        <div className="flex justify-center items-center">
-            <img src={bannerImg} alt="" />
-        </div>
-    );
+  return (
+    <div className="flex justify-center items-center mt-4">
+      {/* <img src={bannerImg} alt="" /> */}
+      {/* <img src={bannerLogo} alt="" /> */}
+      <img src={banner} alt="" />
+    </div>
+  );
 };
 
 export default Banner;
